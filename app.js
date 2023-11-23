14595f5c34154748b8ef0460949dccc0d1b54ecb
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-11-23 18:38:39 +0100

    Added a few features, onClick outside for the dropdowns and more keyboard interaction on the dropdown menu.

--- app.js
+++ app.js
@@ -13,37 +13,15 @@ const checkMarks = document.querySelectorAll(".check");
 const body = document.querySelector(".body");
 document.querySelector(".guideList-len").textContent = guideLists.length;
 
-function handleHiddenSteps(list) {
-  guideLists.forEach((otherList) => {
-    const otherHiddenContent = otherList.querySelector(".guide-lists--hidden");
-    if (
-      otherHiddenContent &&
-      !otherHiddenContent.classList.contains("hidden")
-    ) {
-      otherHiddenContent.classList.add("hidden");
-      otherList.style.background = "";
-      otherList.querySelector("h5").style.fontWeight = "300";
-      otherList.style.transition = "";
-    }
-  });
-
-  list.querySelector(".guide-lists--hidden").classList.remove("hidden");
-  list.style.background = "#f3f3f3";
-  list.querySelector("h5").style.fontWeight = "400";
-  list.style.transition = "background-color 0.3s ease-in-out";
-}
-
-//Functions for clicked menu list
+// Functions for clicked menu list
 function removeActiveClass() {
   links.forEach((link) => {
     link.classList.remove("active-link");
-    link.classList.remove("hover");
   });
 }
 
 links.forEach((link) => {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
     removeActiveClass();
     link.classList.add("active-link");
   });
@@ -75,26 +53,37 @@ notification.addEventListener("click", () => {
 //Event listener to toggle menulist
 storeName.addEventListener("click", () => {
   toggleMenuList();
-});
-
-//Close notification pannel and menu-list at hte press of escape button
-document.addEventListener("keydown", (e) => {
-  switch (e.key) {
-    case "Escape":
-      dropdownPannel.classList.remove("active");
-      menuList.classList.add("hidden");
-      break;
+  if (links.length > 0) {
+    links[0].focus();
   }
 });
 
-// // Add event listener to the document
-document.addEventListener("click", function (event) {
-  // Check if the clicked element is not inside the dropdown
-  if (!event.target.closest(".dropdown-pannel")) {
-    // Close the dropdown
-    if (dropdownPannel) {
-      dropdownPannel.classList.add("hidden");
+// storeName.addEventListener("keydown", (e) => {
+//   if (e.key === "Enter") alert("Enter key pressed!");
+//   if (links.length > 0) {
+//     links[0].focus();
+//   }
+// });
+
+// Event listener to handle keyboard navigation
+menuList.addEventListener("keydown", (e) => {
+  const currentIndex = Array.from(links).indexOf(document.activeElement);
+  removeActiveClass();
+  if (e.key === "ArrowDown") {
+    e.preventDefault();
+    // Move focus to the next item in the menu
+    if (currentIndex < links.length - 1) {
+      links[currentIndex + 1].focus();
     }
+  } else if (e.key === "ArrowUp") {
+    e.preventDefault();
+    // Move focus to the previous item in the menu
+    if (currentIndex > 0) {
+      links[currentIndex - 1].focus();
+    }
+  } else if (e.key === "Enter") {
+    // Replace 'your-site-url' with the actual URL you want to navigate to
+    window.open("https://admin.shopify.com", "_blank");
   }
 });
 
@@ -103,19 +92,61 @@ cancel.addEventListener("click", () => {
   selectPlan.classList.add("hidden");
 });
 
-//Open guide card
-openGuides.addEventListener("click", () => {
+function handleGuideDisplay() {
   openGuides.classList.add("hidden");
   closeGuides.classList.remove("hidden");
   guides.classList.add("hidden");
-});
+}
 
-//Close guide card
-closeGuides.addEventListener("click", () => {
+function handleHideGuides() {
   closeGuides.classList.add("hidden");
   openGuides.classList.remove("hidden");
   guides.classList.remove("hidden");
+}
+
+//Open guide card
+openGuides.addEventListener("click", () => {
+  handleGuideDisplay();
+  console.log("Enter key pressed!", guideLists[0]);
+  guideLists[0].focus();
 });
+
+// openGuides.addEventListener("keydown", (e) => {
+//   if (e.key === "Enter") {
+//     alert("Enter key pressed!");
+//   }
+// });
+
+//Close guide card
+closeGuides.addEventListener("click", () => {
+  handleHideGuides();
+});
+
+// closeGuides.addEventListener("keydown", (e) => {
+//   if (e.key === "Enter") {
+//     alert("Enter key pressed!");
+//   }
+// });
+
+function handleHiddenSteps(list) {
+  guideLists.forEach((otherList) => {
+    const otherHiddenContent = otherList.querySelector(".guide-lists--hidden");
+    if (
+      otherHiddenContent &&
+      !otherHiddenContent.classList.contains("hidden")
+    ) {
+      otherHiddenContent.classList.add("hidden");
+      otherList.style.background = "";
+      otherList.querySelector("h5").style.fontWeight = "300";
+      otherList.style.transition = "";
+    }
+  });
+
+  list.querySelector(".guide-lists--hidden").classList.remove("hidden");
+  list.style.background = "#f3f3f3";
+  list.querySelector("h5").style.fontWeight = "400";
+  list.style.transition = "background-color 0.3s ease-in-out";
+}
 
 //Funtion to display and hide guide steps/content
 function handleListDisplay(clickedList) {
@@ -126,18 +157,14 @@ function handleListDisplay(clickedList) {
   }
 }
 
-//Hanlde click event
+//Hanlde click event to display guides list content
 guideLists.forEach((list) => {
   list.addEventListener("click", () => {
     handleListDisplay(list);
   });
 
-  //Hanlde keydown Enter event
+  //Hanlde keydown Enter event to display guides list content
   list.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-      handleListDisplay(list);
-    }
-
     //Convert from Nodelist to Array and get the index of the current list
     const currentIndex = Array.from(guideLists).indexOf(list);
     switch (e.key) {
@@ -151,6 +178,9 @@ guideLists.forEach((list) => {
         const prevIndex =
           (currentIndex - 1 + guideLists.length) % guideLists.length;
         guideLists[prevIndex].focus();
+        break;
+      case "Enter":
+        e.key === "Enter";
         break;
     }
   });
@@ -226,4 +256,25 @@ checkMarks.forEach((check) => {
   });
 
   check.setAttribute("tabindex", "0");
+});
+
+//Close notification pannel and menu-list at the press of escape button
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "Escape":
+      dropdownPannel.classList.remove("active");
+      menuList.classList.add("hidden");
+      break;
+  }
+});
+
+//Close notification pannel and menu-list on click outside
+document.addEventListener("click", function (event) {
+  // Check if the clicked element is not inside the dropdown
+  if (!event.target.closest(".notification")) {
+    dropdownPannel.classList.remove("active");
+  }
+  if (!event.target.closest(".store-name-wrapper")) {
+    menuList.classList.add("hidden");
+  }
 });
